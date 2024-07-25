4e6be0cbce614a0603df6b239bbbea21355674a8
Miki18 <mikiadam24@wp.pl>
2024-07-26 00:18:17 +0200

    Installed opengl and imgui. Wrote an example program using those libraries

--- TheFish/Game.cpp
+++ TheFish/Game.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
-using namespace std;
+//opengl version 3.4
+//imgui version 1.90.9
 
-int main()
-{
-	cout << "hello world";
+int main() {
+    //initialization glfw
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    //create window
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    //Imgui initialization
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+
+    //Main loop
+    while (!glfwWindowShouldClose(window)) {
+        //clear color
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Imgui example
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::Begin("Hello world!");
+        ImGui::Text("Hello world!");
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+    
+    //terminate
+    glfwTerminate();
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    return 0;
 }
